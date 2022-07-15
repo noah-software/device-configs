b1426556f1151d5d60edc57efb8090310ed722b9
Alexander Frischbutter <afrischbutter@gmail.com>
2022-07-15 14:38:42 +0200

    Update tektelic_mutli_kona_nodered.js

--- tektelic_mutli_kona_nodered.js
+++ tektelic_mutli_kona_nodered.js
@@ -9,23 +9,23 @@ if(msg.payload.uplink_message && msg.payload.uplink_message.decoded_payload) {
         // https://gist.github.com/fincha/b6484b7931e8f828fdc752b09e4e1e14
         
     }
-    else if(msg.payload.uplink_message.decoded_payload.bytes
-        && msg.payload.uplink_message.decoded_payload.acceleration_x) {
+    else if('bytes' in msg.payload.uplink_message.decoded_payload
+        && 'acceleration_x' in msg.payload.uplink_message.decoded_payload) {
         
         // tektelic devices (move, humidity, etc)
         Object.keys(msg.payload.uplink_message.decoded_payload).forEach(key => {
             if(key === "humidity" || key === "moisture"
-                 || key === "temperature") {
-                return;
+                 || key === "temperature" || key === "closed") {
+                node.send({
+                    topic: `${collectionName}/lora/0/${deviceId}/${key}`,
+                    payload: {
+                        val: msg.payload.uplink_message.decoded_payload[key],
+                        ts: Date.now()
+                    }
+                })
             }
             
-            node.send({
-                topic: `${collectionName}/lora/0/${deviceId}/${key}`,
-                payload: {
-                    val: msg.payload.uplink_message.decoded_payload[key],
-                    ts: Date.now()
-                }
-            })
+            
         });
     }
     // TODOL what kind of device is this???
